671662a03d8dd7befae5f1195cb6d63092f9e7fe
Andres Villagra <andres_ve@icloud.com>
2016-06-01 11:46:57 -0300

    Started the parser.py file with it's respective docstring. Updated the user un the UML model

---
+++ JSONparser/parser.py
@@ -0,0 +1,17 @@
+import json
+import sys
+
+'''
+This script does a parsing of the JSON file containing the file to be rendered
+taking the file name as a commandline argument, the JSON format is detailed in
+the model.mdj file under the "JSON Activity" diagram.
+
+This file generates an output of the same name as the original file name with
+the "*.tex" extention marking that it was processed and is ready to be rendered
+'''
+
+def main():
+
+
+if __name__ == "__main__":
+  main()
