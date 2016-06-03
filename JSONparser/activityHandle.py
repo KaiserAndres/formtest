42d3891953b44a3f558ee7481847ca29cd4eda13
Andres Villagra <andres_ve@icloud.com>
2016-06-02 23:38:36 -0300

    Updated the model and added a Header class.

--- JSONparser/activityHandle.py
+++ JSONparser/activityHandle.py
@@ -6,6 +6,12 @@ class File:
     self.header = header
     self.data = []
 
+  def linkHeader(self, header):
+    self.header = header
+
+  def linkMetadata(self, metadata):
+    self.metadata = metadata
+
   def linkData(self, element):
     self.data.append(element)
 
@@ -13,12 +19,13 @@ class File:
     self.data.remove(element)
 
 class MetaData:
-  def __init__(self, userID, language, creationDate, lastChangeDate, fileID):
-    self.userID = userID
-    self.language = language
-    self.creationDate = creationDate
+  def __init__(self, userID = None, language = None, creationDate = None,
+               lastChangeDate = None, fileID = None):
+    self.userID         = userID
+    self.language       = language
+    self.creationDate   = creationDate
     self.lastChangeDate = lastChangeDate
-    self.fileID = fileID
+    self.fileID         = fileID
 
   def setUserID(self, newID):
     self.userID = newID
@@ -35,12 +42,40 @@ class MetaData:
   def setFileID(self, newFileID):
     self.fileID = newFileID
 
+  def updateLastChangeDate(self):
+    self.setLastChange()
+
+class header:
+  def __init__(self, organisation = None, hasHeader = None,
+               hasStudentName = None, hasTeacherName = None,
+               hasFillableDate = None):
+    self.organisation     = organisation
+    self.hasHeader        = hasHeader
+    self.hasStudentName   = hasStudentName
+    self.hasTeacherName   = hasTeacherName
+    self.hasFillableDate  = hasFillableDate
+
+  def setOrganisation(self, organisation):
+    self.organisation = organisation
+
+  def setHasHeader(self, hasHeader):
+    self.hasHeader = hasHeader
+
+  def setHasStudentName(self, hasStudentName):
+    self.hasStudentName = hasStudentName
+
+  def setHasTeacherName(self, hasTeacherName):
+    self.hasTeacherName = hasTeacherName
+
+  def setHasFillableDate(self, hasFillableDate):
+    self.hasFillableDate = hasFillableDate
+
 class Data:
   pass
 
 class TextBlock(Data):
   def __init__(self, baseText):
-    self.textBody = baseText
+    self.textBody      = baseText
     self.subActivities = []
 
   def setText(self, text):
@@ -49,5 +84,22 @@ class TextBlock(Data):
   def linkActivity(self, activity):
     self.subActivities.append(activity)
 
+  def unlinkSubactivity(self, activity):
+    self.subActivities.remove(activity)
+
 class Activity(Data):
   pass
+
+class TrueOrFalse(Activity):
+  def __init__(self, text = None):
+    self.text    = text
+    self.answers = []
+
+
+
+
+
+
+
+
+
